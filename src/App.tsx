07dbbf8204c81ebd60ea6a617784295571f72707
fellipeutaka <fellipeutaka@gmail.com>
2022-01-27 19:55:35 -0300

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import UseState from "./components/UseState";
 import UseEffect from "./components/UseEffect";
 import UseRef from "./components/UseRef";
+import UseReducer from "./components/UseReducer";
 
 export default function App() {
   return (
     <>
       {/* <UseState /> */}
       {/* <UseEffect /> */}
-      <UseRef />
+      {/* <UseRef /> */}
+      <UseReducer />
     </>
   );
 }
---
+++ src/components/UseReducer/index.tsx
@@ -0,0 +1,58 @@
+import { useReducer } from "react";
+import { Container } from "./styles";
+
+const initialState = { count: 0 };
+
+enum CountActionKind {
+  INCREASE = "increment",
+  DECREASE = "decrement",
+}
+
+interface State {
+  count: number;
+}
+
+interface CountAction {
+  type: CountActionKind;
+  payload: number;
+}
+
+function reducer({ count }: State, { type, payload }: CountAction) {
+  switch (type) {
+    case CountActionKind.INCREASE:
+      return { count: count + payload };
+    case CountActionKind.DECREASE:
+      return { count: count - payload };
+    default:
+      throw new Error();
+  }
+}
+
+export default function UseReducer() {
+  const [{ count }, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <Container>
+      <h1>useReducer</h1>
+      <div className="count">
+        <span>Count: {count}</span>
+        <div>
+          <button
+            onClick={() =>
+              dispatch({ type: CountActionKind.DECREASE, payload: 1 })
+            }
+          >
+            -
+          </button>
+          <button
+            onClick={() =>
+              dispatch({ type: CountActionKind.INCREASE, payload: 5 })
+            }
+          >
+            +
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+}
